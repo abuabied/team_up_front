fbd53625817d2a60d286e9be071a8f840ad120ec
Mohamad AbuAbied <82101799+abuabied@users.noreply.github.com>
2023-09-08 05:03:26 +0300

    Created Navigation Bar

---
+++ src/components/header/MobileSideMenu.jsx
@@ -0,0 +1,91 @@
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import MailIcon from "@mui/icons-material/Mail";
+import { useState } from "react";
+
+export const MobileSideMenu = () => {
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  const mobileMenuId = "primary-search-account-menu-mobile";
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton size="large" aria-label="show new inbox" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <Box sx={{ display: { xs: "flex", md: "none" } }}>
+      <IconButton
+        size="large"
+        aria-label="show more"
+        aria-controls={mobileMenuId}
+        aria-haspopup="true"
+        onClick={handleMobileMenuOpen}
+        color="inherit"
+      >
+        <MoreIcon />
+      </IconButton>
+      {renderMobileMenu}
+    </Box>
+  );
+};
---
+++ src/components/header/NavBar.jsx
@@ -0,0 +1,37 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { SearchBar } from "./SearchBar";
+import { DesktopSideMenu } from "./DesktopSideMenu";
+import { MobileSideMenu } from "./MobileSideMenu";
+import SetWindowSize from "../../helpers/setWindowSize";
+
+export const NavBar = () => {
+  const windowSize = SetWindowSize();
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar sx={{ background: "black" }}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <SearchBar />
+
+          <Box sx={{ flexGrow: 1 }} />
+
+          {windowSize >= 900 ? <DesktopSideMenu /> : <MobileSideMenu />}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
