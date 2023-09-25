ee36ee0c07bc6b13e2cadbfb42b0420dbb798d13
Mohamad AbuAbied <82101799+abuabied@users.noreply.github.com>
2023-09-25 20:06:56 +0300

    update

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "https://ec2-13-50-159-25.eu-north-1.compute.amazonaws.com:8080/api/v1",
+    "https://ec2-13-50-159-25.eu-north-1.compute.amazonaws.com:443/api/v1",
   headers: { "ngrok-skip-browser-warning": "true" },
   withCredentials: true,
 });
