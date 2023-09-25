a1c9c746c75acd43975ab99272abcb30b1a5a3db
Mohamad AbuAbied <82101799+abuabied@users.noreply.github.com>
2023-09-25 16:44:54 +0300

    update server api

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,13 +1,3 @@
-<<<<<<< Updated upstream
-import axios from 'axios';
-
-export default axios.create({
-  baseURL:
-    "http://ec2-13-53-125-111.eu-north-1.compute.amazonaws.com:8080/api/v1",
-  headers: { "ngrok-skip-browser-warning": "true" },
-  withCredentials: true,
-});
-=======
 import axios from "axios";
 
 export default axios.create({
@@ -16,4 +6,3 @@ export default axios.create({
   headers: { "ngrok-skip-browser-warning": "true" },
   withCredentials: true,
 });
->>>>>>> Stashed changes
